f0b19dad8c0db05b328f928b0ee18fc2264d545f
quaintcabeza <ramyaiyer22@gmail.com>
2016-04-14 00:10:41 -0400

    Text change listener works

--- app/src/main/java/quaintcabeza/dhurihunt/ClueSeeker.java
+++ app/src/main/java/quaintcabeza/dhurihunt/ClueSeeker.java
@@ -3,17 +3,53 @@ package quaintcabeza.dhurihunt;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class ClueSeeker extends ActionBarActivity {
 
+    private EditText d_passPhraseView;
+    private Button d_leftButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("FOO", "onCreate");
         setContentView(R.layout.activity_clue_seeker);
+        d_leftButton = (Button) findViewById(R.id.left_button);
+        d_passPhraseView = (EditText) findViewById(R.id.pass_phrase_text);
+        d_passPhraseView.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                Log.e("FOO", s.toString());
+                if (d_passPhraseView.getText().toString().equals("ffff")) {
+                    d_leftButton.setEnabled(false);
+                }
+            }
+
+            public void beforeTextChanged(
+                    CharSequence s,
+                    int start,
+                    int count,
+                    int after) {
+            }
+
+            public void onTextChanged(
+                    CharSequence s,
+                    int start,
+                    int before,
+                    int count) {
+                Log.e("FOO", s.toString());
+                if (s.toString() == "asdf") {
+                    d_leftButton.setEnabled(false);
+                }
+            }
+        });
     }
 
     @Override
@@ -36,6 +72,11 @@ public class ClueSeeker extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onLeftClick(View view) {
+        Intent intent = new Intent(this, Playback.class);
+        startActivity(intent);
     }
 
     public void onRightClick(View view) {
